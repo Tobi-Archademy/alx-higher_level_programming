9233c505bc561c58612664a18ba8c67eae0c5117
Oluwatobi <oluwatobiabass5@gmail.com>
2021-09-28 06:45:59 -0700

    added an area of square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+''' Define a class Square. '''
 class Square:
+    ''' Represent a class Square '''
     def __init__(self, size=0):
+        """Initialize a new Square method.
+        
+        Args:
+            __size (int): The __size of the new square.
+        """
         if not str(size).isdigit():
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -8,5 +15,10 @@ class Square:
         else:
             self.__size = size
 
+    ''' Initialize an area of the Square method.
+
+    Args:
+        self: calculating the area of the size of Square.
+    '''
     def area(self):
         return int(self.__size) ** 2
