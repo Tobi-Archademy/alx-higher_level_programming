4d9f63d93520dfa4cbabfe5080c68be1473b1880
Oluwatobi <oluwatobiabass5@gmail.com>
2021-09-28 12:17:17 -0700

    added the area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 ''' Define a class Square. '''
+
+
 class Square:
     ''' Represent a class Square '''
+
     def __init__(self, size=0):
         """Initialize a new Square method.
-        
+
         Args:
             __size (int): The __size of the new square.
         """
@@ -15,10 +18,6 @@ class Square:
         else:
             self.__size = size
 
-    ''' Initialize an area of the Square method.
-
-    Args:
-        self: calculating the area of the size of Square.
-    '''
+    '''Return the current area of the square.'''
     def area(self):
         return int(self.__size) ** 2
