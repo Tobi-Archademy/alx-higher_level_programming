c266c8b20da2cf6fc589eea1cf5db87b543268ed
Oluwatobi <oluwatobiabass5@gmail.com>
2021-09-28 12:21:55 -0700

    added a function that prints a square

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 ''' Define a class Square. '''
+
+
 class Square:
     ''' Represent a class Square '''
     def __init__(self, size=0):
         self.__size = size
         """Initialize a new Square method.
-        
+
         Args:
             __size (int): The __size of the new square.
         """
