4443482c8e7f2f93cd69c01a26169c64d6fdee3a
Oluwatobi <oluwatobiabass5@gmail.com>
2021-09-28 12:01:06 -0700

    added a Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-# 1-square.py
 
 """Define a class Square."""
 
+
 class Square:
     ''' Represents an square class '''
+
     def __init__(self, size):
         '''Initialize a new square.
 
