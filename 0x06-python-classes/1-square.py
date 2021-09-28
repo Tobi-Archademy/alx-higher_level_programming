5e74648c823d8e5158e8d2e150fa066a1c34cb35
Oluwatobi <oluwatobiabass5@gmail.com>
2021-09-28 06:17:51 -0700

    added a class Square that defines a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 1-square.py
+
+"""Define a class Square."""
+
+class Square:
+    ''' Represents an square class '''
+    def __init__(self, size):
+        '''Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        '''
+        self.__size = size
