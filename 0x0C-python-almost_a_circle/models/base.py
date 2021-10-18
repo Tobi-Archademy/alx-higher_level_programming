0186d8f22e64f79af1d5e3e9dc12a14270ee9ec9
Oluwatobi <oluwatobiabass5@gmail.com>
2021-10-18 13:26:03 -0700

    updated the base class infusing json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,3 +24,18 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) is 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ writes the JSON string to a file """
+        temp = []
+        if list_objs is None or len(list_objs) is 0:
+            pass
+        else:
+            for i in list_objs:
+                temp.append(cls.to_dictionary(i))
+        x = cls.to_json_string(temp)
+        with open("{}.json".format(cls.__name__),
+                  mode='w', encoding='utf-8') as f:
+            f.write(x)
