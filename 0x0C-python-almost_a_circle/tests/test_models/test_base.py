e2e7875b382b3a96e4a4fc98c39989d0e3d2c8be
Oluwatobi <oluwatobiabass5@gmail.com>
2021-10-18 12:53:56 -0700

    updated tests for base class

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -3,6 +3,8 @@
 
 from models.base import Base
 import unittest
+import json
+import pep8
 
 
 class Test_Base(unittest.TestCase):
@@ -19,6 +21,21 @@ class Test_Base(unittest.TestCase):
         """ tests for input error """
         with self.assertRaises(AttributeError):
             self.assertIsEqual(x(), 1)
+
+
+    def test_pep8_conformance_base(self):
+        """Test that models/base.py conforms to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/base.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_pep8_conformance_test_base(self):
+        """Test that tests/test_models/test_base.py conforms to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['tests/test_models/test_base.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
     def test_empty(self):
         """ test empty conditions """
