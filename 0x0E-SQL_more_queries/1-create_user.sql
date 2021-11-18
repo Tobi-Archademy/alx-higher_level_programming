f66ca3980e54c5e19767ddcbd37d6b045923e455
Oluwatobi <oluwatobiabass5@gmail.com>
2021-11-17 18:17:33 -0800

    added a script that creates the MySQL server user user_0d_1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,5 +2,5 @@
 -- Grants user_od_1 all privileges
 -- If user_od_1 already exist, script would not fail
 
-CREATE USER IF NOT EXIST 'user_od_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO 'user_od_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_od_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_od_1'@'localhost';
